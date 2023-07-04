6a488029af7f9e1c3ea16e77ce60e615c39a2721
phaneendra97 <phaneendra1997@gmail.com>
2023-07-03 18:43:44 -0700

    added discussion

--- pfsi/lib/discussionPages/discussion.dart
+++ pfsi/lib/discussionPages/discussion.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:pfsi/authPages/signup.dart';
 import 'package:pfsi/discussionPages/discussionAdd.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:pfsi/discussionPages/discussionView.dart';
 
 void main() => runApp(const DiscussionPage());
 
@@ -13,12 +17,14 @@ class DiscussionPage extends StatefulWidget {
 
 class _DiscussionPageState extends State<DiscussionPage>
     with TickerProviderStateMixin {
-  var isDialOpen = ValueNotifier<bool>(false);
+  final User? user = FirebaseAuth.instance.currentUser;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final int _perPage = 10; // Number of documents to fetch per page
+  DocumentSnapshot? _lastDocument; // Last fetched document for pagination
   String _selectedOption = 'All discussions';
   @override
-  List<String> items = List.generate(20, (index) => 'Item $index');
   Widget build(BuildContext context) {
-    print(items);
+    String? uuid = user?.uid;
 
     void logout() async {
       try {
@@ -34,94 +40,126 @@ class _DiscussionPageState extends State<DiscussionPage>
       }
     }
 
-    return WillPopScope(
-        onWillPop: () async {
-          if (isDialOpen.value) {
-            isDialOpen.value = false;
-            return false;
-          }
-          return true;
-        },
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text(
-              'Discussion',
+    Stream<QuerySnapshot> _fetchData() {
+      Query query = _firestore
+          .collection('discussion_list')
+          .orderBy('dateTimestamp',
+              descending: true) // Order by a field for pagination
+          .limit(_perPage); // Limit the number of documents per page
+      if (_selectedOption == 'Your discussions') {
+        query.where('userid', isEqualTo: uuid);
+      }
+      if (_lastDocument != null) {
+        query = query.startAfterDocument(_lastDocument!);
+      }
+      return query.snapshots();
+    }
+
+    void _loadMoreData() {
+      final snapshots = _fetchData();
+      snapshots.listen((snapshot) {
+        if (snapshot.docs.length > 0) {
+          _lastDocument = snapshot.docs[snapshot.docs.length - 1];
+        }
+      });
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Discussion',
+          style: TextStyle(color: Colors.white),
+        ),
+        actions: [
+          TextButton.icon(
+            onPressed: logout,
+            icon: Icon(
+              Icons.logout,
+              color: Colors.white,
+            ),
+            label: Text(
+              'Logout',
               style: TextStyle(color: Colors.white),
             ),
-            actions: [
-              TextButton.icon(
-                onPressed: logout,
-                icon: Icon(
-                  Icons.logout,
-                  color: Colors.white,
-                ),
-                label: Text(
-                  'Logout',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            ],
-            backgroundColor: Colors.red[300],
           ),
-          body: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              children: [
-                SizedBox(height: 10),
-                DropdownButton<String>(
-                  value: _selectedOption,
-                  onChanged: (String? newValue) {
-                    setState(() {
-                      _selectedOption = newValue!;
-                    });
-                  },
-                  items: <String>[
-                    'All discussions',
-                    'Your discussions',
-                  ].map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                ),
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: items.length,
-                    itemBuilder: (BuildContext context, int index) {
+        ],
+        backgroundColor: Colors.red[300],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            SizedBox(height: 10),
+            DropdownButton<String>(
+              value: _selectedOption,
+              onChanged: (String? newValue) {
+                setState(() {
+                  _selectedOption = newValue!;
+                });
+                _lastDocument = null;
+                _fetchData();
+              },
+              items: <String>[
+                'All discussions',
+                'Your discussions',
+              ].map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+            Flexible(
+                child: StreamBuilder<QuerySnapshot>(
+              stream: _fetchData(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  final documents = snapshot.data!.docs;
+                  return ListView.builder(
+                    itemCount: documents.length,
+                    itemBuilder: (context, index) {
+                      final data =
+                          documents[index].data() as Map<String, dynamic>;
                       return Column(children: [
                         InkWell(
-                          splashColor: Colors.red,
-                          hoverColor: Colors.red[200],
-                          onTap: () {
-                            print('Item clicked: ${items[index]}');
-                          },
-                          child: ListTile(
-                            title: Text(
-                              items[index],
-                            ),
-                            subtitle: Text('Subtitle for ${items[index]}'),
-                          ),
-                        ),
+                            splashColor: Colors.red,
+                            hoverColor: Colors.red[200],
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        DiscussionView(documents[index].id)),
+                              );
+                            },
+                            child: ListTile(
+                              title: Text(data['topic']),
+                            )),
                         Divider()
                       ]);
                     },
-                  ),
-                ),
-              ],
-            ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => DiscussionAdd()),
-              );
-            },
-            backgroundColor: Colors.red[300],
-            tooltip: 'Add Discussion',
-            child: const Icon(Icons.create_outlined),
-          ),
-        ));
+                  );
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else {
+                  return CircularProgressIndicator();
+                }
+              },
+            ))
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => DiscussionAdd()),
+          );
+        },
+        backgroundColor: Colors.red[300],
+        tooltip: 'Add Discussion',
+        child: const Icon(Icons.create_outlined),
+      ),
+    );
   }
 }
---
+++ pfsi/lib/discussionPages/discussionView.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:pfsi/authPages/signup.dart';
+import 'package:pfsi/commonNavigation/commonNavigation.dart';
+
+class DiscussionView extends StatelessWidget {
+  String documentId = '';
+  DiscussionView(String documentId) {
+    this.documentId = documentId;
+  }
+  TextEditingController _questionController = TextEditingController();
+  TextEditingController _topicController = TextEditingController();
+
+  // Replace 'your_collection' with the name of your collection in Firestore
+// Replace 'your_document_id' with the ID of the document you want to retrieve
+  // Future<void> getDocument() async {
+  //   FirebaseFirestore.instance
+  //       .collection('discussion_list')
+  //       .doc('QC1FGo4WqoSM3BdxQUVD')
+  //       .get()
+  //       .then((DocumentSnapshot documentSnapshot) {
+  //     if (documentSnapshot.exists) {
+  //       print('Document data: ${documentSnapshot.data()}');
+  //     } else {
+  //       print('Document does not exist on the database');
+  //     }
+  //   });
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    print(documentId);
+    void logout() async {
+      try {
+        await FirebaseAuth.instance.signOut();
+        // Perform any additional actions after logout
+        print('Logout successful');
+        Navigator.pop(context);
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => SignupPage()),
+        );
+      } catch (e) {
+        // Handle logout errors
+        print('Logout failed: $e');
+      }
+    }
+
+    void goBack() {
+      Navigator.pop(context);
+    }
+
+    Future<void> addDiscussion() async {
+      final User? user = FirebaseAuth.instance.currentUser;
+      String? uuid = user?.uid;
+      if (uuid != null) {
+        Map<String, dynamic> payload = {
+          "topic": _topicController.text,
+          "question": _questionController.text,
+          "comments": [],
+          "dateTimestamp": DateTime.now(),
+          "userid": uuid
+        };
+        FirebaseFirestore.instance
+            .collection('discussion_list')
+            .add(payload)
+            .then((value) => print("User Added"))
+            .catchError((error) => print("Failed to add user: $error"));
+      } else {
+        print('no uuid');
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => SignupPage()),
+        );
+      }
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Discussion',
+          style: TextStyle(color: Colors.white),
+        ),
+        leading: IconButton(
+            onPressed: goBack,
+            icon: Icon(
+              Icons.arrow_back_outlined,
+              color: Colors.white,
+            )),
+        actions: [
+          TextButton.icon(
+            onPressed: logout,
+            icon: Icon(
+              Icons.logout,
+              color: Colors.white,
+            ),
+            label: Text(
+              'Logout',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
+        backgroundColor: Colors.red[300],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _topicController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter your Topic',
+              ),
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              controller: _questionController,
+              maxLines: null,
+              keyboardType: TextInputType.multiline,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter your question',
+              ),
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                // Get the entered question
+                String question = _questionController.text;
+                String topic = _topicController.text;
+
+                // Submit button logic here
+                addDiscussion();
+                print('Submitted question: $question $topic');
+              },
+              child: Text('Submit'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
