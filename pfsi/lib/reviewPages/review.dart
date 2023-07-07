daef4c161d9c0643b0608e4b7353838f418e313e
phaneendra97 <phaneendra1997@gmail.com>
2023-07-06 23:37:11 -0700

    temp

--- pfsi/lib/reviewPages/review.dart
+++ pfsi/lib/reviewPages/review.dart
@@ -3,7 +3,24 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:pfsi/authPages/signup.dart';
 import 'package:pfsi/reviewPages/reviewAdd.dart';
 
-class Review extends StatelessWidget {
+class Review extends StatefulWidget {
+  const Review({Key? key}) : super(key: key);
+
+  @override
+  _ReviewPageState createState() => _ReviewPageState();
+}
+
+class _ReviewPageState extends State<Review> with TickerProviderStateMixin {
+  String _selectedReviewType = 'All reviews';
+  final ValueNotifier<String> _selectedReviewTypeNotifier =
+      ValueNotifier<String>('All reviews');
+
+  @override
+  void dispose() {
+    _selectedReviewTypeNotifier.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     void logout() async {
@@ -22,52 +39,73 @@ class Review extends StatelessWidget {
     }
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Review',
-            style: TextStyle(color: Colors.white),
-          ),
-          actions: [
-            TextButton.icon(
-              onPressed: logout,
-              icon: Icon(
-                Icons.logout,
-                color: Colors.white,
-              ),
-              label: Text(
-                'Logout',
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-          ],
-          backgroundColor: Colors.red[300],
+      appBar: AppBar(
+        title: Text(
+          'Review',
+          style: TextStyle(color: Colors.white),
         ),
-        body: Center(
-          child: FractionallySizedBox(
-            widthFactor: 0.8,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(height: 10),
-                Text(
-                  'Welcome To Review!',
-                  textAlign: TextAlign
-                      .center, // Align the text within the center of the card
-                ),
-              ],
+        actions: [
+          TextButton.icon(
+            onPressed: logout,
+            icon: Icon(
+              Icons.logout,
+              color: Colors.white,
+            ),
+            label: Text(
+              'Logout',
+              style: TextStyle(color: Colors.white),
             ),
           ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => AddReview()),
-            );
-          },
-          backgroundColor: Colors.red[300],
-          tooltip: 'Add a new Review',
-          child: const Icon(Icons.add_outlined),
-        ));
+        ],
+        backgroundColor: Colors.red[300],
+      ),
+      body: Column(
+        mainAxisAlignment: _selectedReviewType == 'Your reviews'
+            ? MainAxisAlignment.start
+            : MainAxisAlignment.center,
+        children: [
+          SizedBox(width: 20),
+          Align(
+            alignment: Alignment.center,
+            child: ValueListenableBuilder<String>(
+              valueListenable: _selectedReviewTypeNotifier,
+              builder: (context, value, child) {
+                return DropdownButton<String>(
+                  value: value,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _selectedReviewType = newValue!;
+                    });
+                    _selectedReviewTypeNotifier.value = newValue!;
+                  },
+                  items: <String>[
+                    'All reviews',
+                    'Your reviews',
+                  ].map<DropdownMenuItem<String>>(
+                    (String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    },
+                  ).toList(),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AddReview()),
+          );
+        },
+        backgroundColor: Colors.red[300],
+        tooltip: 'Add a new Review',
+        child: const Icon(Icons.add_outlined),
+      ),
+    );
   }
 }
