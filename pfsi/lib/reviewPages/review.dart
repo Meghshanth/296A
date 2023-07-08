8c3d0bd2d8619f4f7828775fefad4170417ae2e1
phaneendra97 <phaneendra1997@gmail.com>
2023-07-08 11:13:58 -0700

    review list

--- pfsi/lib/reviewPages/review.dart
+++ pfsi/lib/reviewPages/review.dart
@@ -20,8 +20,8 @@ Future<String> _loadJsonData() async {
 }
 
 class _ReviewPageState extends State<Review> with TickerProviderStateMixin {
-  String selectedService = "";
-  String selectedRegion = "";
+  String selectedService = "All Services";
+  String selectedRegion = "All Regions";
 
   List<String> serviceOptions = [];
   List<String> regionOptions = [];
@@ -81,28 +81,25 @@ class _ReviewPageState extends State<Review> with TickerProviderStateMixin {
     String? uuid = user?.uid;
 
     Stream<QuerySnapshot>? _fetchData() {
-      if (selectedService != '' &&
-          selectedService != '--Services--' &&
-          selectedRegion != '' &&
-          selectedRegion != '--Region--') {
-        Query query = _firestore
-            .collection('review_list')
-            .orderBy('dateTimestamp', descending: true)
-            .where('service',
-                isEqualTo: selectedService); // Order by a field for pagination
-        query = query.where('region', isEqualTo: selectedRegion);
-        // .limit(_perPage); // Limit the number of documents per page
-        if (_selectedReviewType == 'Your reviews') {
-          query = query.where('userid', isEqualTo: uuid);
-        }
-        return query.snapshots();
-        // return query.snapshots().handleError((error) {
-        //   // Handle the error here
-        //   print('Error fetching data: $error');
-        // });
-      } else {
-        return null;
+      Query query = _firestore
+          .collection('review_list')
+          .orderBy('dateTimestamp', descending: true);
+      // .limit(_perPage); // Limit the number of documents per page
+      if (selectedService != 'All Services') {
+        query = query.where('service', isEqualTo: selectedService);
       }
+      if (selectedRegion != 'All Regions') {
+        query = query.where('region', isEqualTo: selectedRegion);
+      }
+      if (_selectedReviewType == 'Your reviews') {
+        query = query.where('userid', isEqualTo: uuid);
+      }
+      print(query.parameters);
+      // return query.snapshots();
+      return query.snapshots().handleError((error) {
+        // Handle the error here
+        print('Error fetching data: $error');
+      });
     }
 
     void logout() async {
@@ -144,146 +141,150 @@ class _ReviewPageState extends State<Review> with TickerProviderStateMixin {
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
-          mainAxisAlignment: selectedService != '--Services--'
-              ? MainAxisAlignment.start
-              : MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             SizedBox(width: 20),
             Align(
                 alignment: Alignment.center,
-                child: Column(children: [
-                  ValueListenableBuilder<String>(
-                    valueListenable: _selectedReviewTypeNotifier,
-                    builder: (context, value, child) {
-                      return DropdownButtonFormField<String>(
-                        decoration: InputDecoration(
-                          labelText: 'Select review type',
-                        ),
-                        value: value,
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            _selectedReviewType = newValue!;
-                          });
-                          _selectedReviewTypeNotifier.value = newValue!;
-                        },
-                        items: <String>[
-                          'All reviews',
-                          'Your reviews',
-                        ].map<DropdownMenuItem<String>>(
-                          (String value) {
-                            return DropdownMenuItem<String>(
-                              value: value,
-                              child: Text(value),
-                            );
-                          },
-                        ).toList(),
-                      );
-                    },
-                  ),
-                  DropdownButtonFormField<String>(
-                    value: selectedRegion,
-                    items: regionOptions.map((option) {
-                      return DropdownMenuItem<String>(
-                        value: option,
-                        child: Text(option),
-                      );
-                    }).toList(),
-                    onChanged: (value) {
-                      setState(() {
-                        if (regionOptions[0] == '--Region--') {
-                          regionOptions.removeAt(0);
-                        }
-                        selectedRegion = value!;
-                      });
-                    },
-                    decoration: InputDecoration(
-                      labelText: 'Select a region',
-                    ),
-                  ),
-                  DropdownButtonFormField<String>(
-                    value: selectedService,
-                    items: serviceOptions.map((option) {
-                      return DropdownMenuItem<String>(
-                        value: option,
-                        child: Text(option),
-                      );
-                    }).toList(),
-                    onChanged: (value) {
-                      setState(() {
-                        if (serviceOptions[0] == '--Services--') {
-                          serviceOptions.removeAt(0);
-                        }
-                        selectedService = value!;
-                      });
-                    },
-                    decoration: InputDecoration(
-                      labelText: 'Select a service',
-                    ),
-                  ),
-                ])),
-            if (selectedService != '--Services--')
-              Flexible(
-                child: ValueListenableBuilder<String>(
-                  valueListenable: _selectedReviewTypeNotifier,
-                  builder: (context, selectedOption, child) {
-                    return StreamBuilder<QuerySnapshot>(
-                      stream: _fetchData(),
-                      builder: (context, snapshot) {
-                        if (snapshot.hasData) {
-                          final documents = snapshot.data!.docs;
-                          if (documents.isNotEmpty) {
-                            return ListView.builder(
-                              itemCount: documents.length,
-                              itemBuilder: (context, index) {
-                                final data = documents[index].data()
-                                    as Map<String, dynamic>;
-                                return Column(
-                                  children: [
-                                    InkWell(
-                                      splashColor: Colors.red,
-                                      hoverColor: Colors.red[200],
-                                      onTap: () {
-                                        Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                            builder: (context) =>
-                                                DiscussionView(
-                                              documents[index].id,
-                                            ),
-                                          ),
-                                        );
-                                      },
-                                      child: ListTile(
-                                        title: Text(data['businessName']),
-                                      ),
-                                    ),
-                                    Divider(),
-                                  ],
-                                );
-                              },
-                            );
-                          } else {
-                            return Card(
-                              margin: EdgeInsets.only(top: 16),
-                              child: Padding(
-                                padding: EdgeInsets.all(16),
-                                child: Text(
-                                  'No reviews found for this category',
-                                  style: TextStyle(fontSize: 16),
+                child: Card(
+                    margin: EdgeInsets.only(top: 16),
+                    child: Padding(
+                        padding: EdgeInsets.all(16),
+                        child: Column(children: [
+                          Text(
+                            'Filters',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          ValueListenableBuilder<String>(
+                            valueListenable: _selectedReviewTypeNotifier,
+                            builder: (context, value, child) {
+                              return DropdownButtonFormField<String>(
+                                decoration: InputDecoration(
+                                  labelText: 'Select review type',
                                 ),
-                              ),
-                            );
-                          }
-                        } else if (snapshot.hasError) {
-                          return Text('Error!!!: ${snapshot.error}');
+                                value: value,
+                                onChanged: (String? newValue) {
+                                  setState(() {
+                                    _selectedReviewType = newValue!;
+                                  });
+                                  _selectedReviewTypeNotifier.value = newValue!;
+                                },
+                                items: <String>[
+                                  'All reviews',
+                                  'Your reviews',
+                                ].map<DropdownMenuItem<String>>(
+                                  (String value) {
+                                    return DropdownMenuItem<String>(
+                                      value: value,
+                                      child: Text(value),
+                                    );
+                                  },
+                                ).toList(),
+                              );
+                            },
+                          ),
+                          DropdownButtonFormField<String>(
+                            value: selectedRegion,
+                            items: regionOptions.map((option) {
+                              return DropdownMenuItem<String>(
+                                value: option,
+                                child: Text(option),
+                              );
+                            }).toList(),
+                            onChanged: (value) {
+                              setState(() {
+                                selectedRegion = value!;
+                              });
+                            },
+                            decoration: InputDecoration(
+                              labelText: 'Select a region',
+                            ),
+                          ),
+                          DropdownButtonFormField<String>(
+                            value: selectedService,
+                            items: serviceOptions.map((option) {
+                              return DropdownMenuItem<String>(
+                                value: option,
+                                child: Text(option),
+                              );
+                            }).toList(),
+                            onChanged: (value) {
+                              setState(() {
+                                selectedService = value!;
+                              });
+                            },
+                            decoration: InputDecoration(
+                              labelText: 'Select a service',
+                            ),
+                          ),
+                        ])))),
+            SizedBox(
+              height: 10.0,
+            ),
+            Flexible(
+              child: ValueListenableBuilder<String>(
+                valueListenable: _selectedReviewTypeNotifier,
+                builder: (context, selectedOption, child) {
+                  return StreamBuilder<QuerySnapshot>(
+                    stream: _fetchData(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        final documents = snapshot.data!.docs;
+                        if (documents.isNotEmpty) {
+                          return ListView.builder(
+                            itemCount: documents.length,
+                            itemBuilder: (context, index) {
+                              final data = documents[index].data()
+                                  as Map<String, dynamic>;
+                              return Column(
+                                children: [
+                                  InkWell(
+                                    splashColor: Colors.red,
+                                    hoverColor: Colors.red[200],
+                                    onTap: () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) => DiscussionView(
+                                            documents[index].id,
+                                          ),
+                                        ),
+                                      );
+                                    },
+                                    child: ListTile(
+                                      title: Text(data['businessName']),
+                                    ),
+                                  ),
+                                  Divider(),
+                                ],
+                              );
+                            },
+                          );
                         } else {
-                          return CircularProgressIndicator();
+                          return Card(
+                            margin: EdgeInsets.only(top: 16),
+                            child: Padding(
+                              padding: EdgeInsets.all(16),
+                              child: Text(
+                                'No reviews found for this category',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ),
+                          );
                         }
-                      },
-                    );
-                  },
-                ),
+                      } else if (snapshot.hasError) {
+                        return Text('Error!!!: ${snapshot.error}');
+                      } else {
+                        return CircularProgressIndicator();
+                      }
+                    },
+                  );
+                },
               ),
+            ),
           ],
         ),
       ),
